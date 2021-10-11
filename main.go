98acd812f1bbdbf3af6c38b12839f527b6caf205
Fabien Zucchet <fabienz@padok.fr>
2021-10-11 23:56:22 +0200

    feat(tsdb): add influxdb time series database to store the availability and response time

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"github.com/influxdata/influxdb-client-go/v2"
 	"log"
 	"net/http"
 	"sync"
 	"time"
 )
 
-func check(wg *sync.WaitGroup, target string) {
+func check(wg *sync.WaitGroup, target string, interval int) {
+
+	// You can generate a Token from the "Tokens Tab" in the UI
+	const token = "<token here>"
+	const bucket = "go-monitoring-tool"
+	const org = "fabienzucchet"
+
+	client := influxdb2.NewClient("http://localhost:8086", token)
+	// always close client at the end
+	defer client.Close()
+
+	// get non-blocking write client
+	writeAPI := client.WriteAPI(org, bucket)
+
 	defer wg.Done()
 
-	start := time.Now()
-	_, err := http.Get(target)
-	if err != nil {
-		log.Println("Target :", target, "is not healthy ! ❌")
-	} else {
-		elapsed := time.Since(start)
-		log.Println("Target :", target, "is healthy and responded in", elapsed, "! ✅")
+	for range time.Tick(time.Duration(interval) * time.Second) {
+		start := time.Now()
+		_, err := http.Get(target)
+		if err != nil {
+			log.Println("Target :", target, "is not healthy ! ❌")
+			p := influxdb2.NewPointWithMeasurement("healthcheck").AddTag("target", target).AddField("status", 0).SetTime(time.Now())
+			// write point asynchronously
+			writeAPI.WritePoint(p)
+		} else {
+			elapsed := time.Since(start)
+			log.Println("Target :", target, "is healthy and responded in", elapsed, "! ✅")
+			p := influxdb2.NewPointWithMeasurement("healthcheck").AddTag("target", target).AddField("status", 1).AddField("response_time", float32(elapsed)).SetTime(time.Now())
+			// write point asynchronously
+			writeAPI.WritePoint(p)
+		}
+		// Flush writes
+		writeAPI.Flush()
 	}
 }
 
@@ -24,14 +48,13 @@ func main() {
 
 	targets := []string{"http://localhost:8080", "https://google.fr", "https://padok.fr"}
 
-	// Init a wait group
 	var wg sync.WaitGroup
 
 	for _, target := range targets {
 		wg.Add(1)
-		go check(&wg, target)
+		go check(&wg, target, 15)
 	}
 
-	// Wait for the waiting group to end
 	wg.Wait()
+
 }
