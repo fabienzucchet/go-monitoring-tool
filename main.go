7f1c5685bcbd9075279c5adf431349a49fba0268
Fabien Zucchet <fabienz@padok.fr>
2021-10-12 21:28:06 +0200

    refactor(config): refactor influx db config and use environment variable for the token

--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-	"github.com/influxdata/influxdb-client-go/v2"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
-func check(wg *sync.WaitGroup, target string, interval int) {
+type InfluxConfig struct {
+	token string
+	bucket string
+	org string
+	url string
+}
 
-	// You can generate a Token from the "Tokens Tab" in the UI
-	const token = "<token here>"
-	const bucket = "go-monitoring-tool"
-	const org = "fabienzucchet"
 
-	client := influxdb2.NewClient("http://localhost:8086", token)
+func check(target string, interval int, influxConfig *InfluxConfig) {
+
+	client := influxdb2.NewClient(influxConfig.url, influxConfig.token)
 	// always close client at the end
 	defer client.Close()
 
 	// get non-blocking write client
-	writeAPI := client.WriteAPI(org, bucket)
-
-	defer wg.Done()
+	writeAPI := client.WriteAPI(influxConfig.org, influxConfig.bucket)
 
 	for range time.Tick(time.Duration(interval) * time.Second) {
 		start := time.Now()
-		_, err := http.Get(target)
+		resp, err := http.Get(target)
 		if err != nil {
 			log.Println("Target :", target, "is not healthy ! ❌")
 			p := influxdb2.NewPointWithMeasurement("healthcheck").AddTag("target", target).AddField("status", 0).SetTime(time.Now())
@@ -35,7 +38,7 @@ func check(wg *sync.WaitGroup, target string, interval int) {
 		} else {
 			elapsed := time.Since(start)
 			log.Println("Target :", target, "is healthy and responded in", elapsed, "! ✅")
-			p := influxdb2.NewPointWithMeasurement("healthcheck").AddTag("target", target).AddField("status", 1).AddField("response_time", float32(elapsed)).SetTime(time.Now())
+			p := influxdb2.NewPointWithMeasurement("healthcheck").AddTag("target", target).AddField("status", 1).AddField("response_time", float32(elapsed)).AddField("status_code", int(resp.StatusCode)).SetTime(time.Now())
 			// write point asynchronously
 			writeAPI.WritePoint(p)
 		}
@@ -46,15 +49,25 @@ func check(wg *sync.WaitGroup, target string, interval int) {
 
 func main() {
 
-	targets := []string{"http://localhost:8080", "https://google.fr", "https://padok.fr"}
-
-	var wg sync.WaitGroup
-
-	for _, target := range targets {
-		wg.Add(1)
-		go check(&wg, target, 15)
+	influxConfig := InfluxConfig{
+		token: os.Getenv("TSDB_TOKEN"),
+		bucket: "go-monitoring-tool",
+		org: "fabienzucchet",
+		url: "http://localhost:8086",
 	}
 
+	targets := []string{"http://localhost:8080", "https://google.fr", "https://padok.fr"}
+
+	log.Println("Starting the monitoring tool... ⌛️")
+
+	for _, target := range targets {
+		go check(target, 15, &influxConfig)
+	}
+
+	log.Println("Started the monitoring tool. ⚡️")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
 	wg.Wait()
 
 }
